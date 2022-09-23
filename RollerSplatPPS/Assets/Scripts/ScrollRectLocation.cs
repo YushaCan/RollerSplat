39e43f827fb6e61878ddb69447d7245cb3ec67f3
YusaCan <ahmedyusacan@gmail.com>
2022-09-23 17:34:54 +0300

    ScrollRectLocation Script Created

---
+++ RollerSplatPPS/Assets/Scripts/ScrollRectLocation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollRectLocation : MonoBehaviour
+{
+    public ScrollRect scrollRect;
+    public ScriptableInt currentLevel, totalLevel;
+    void Start()
+    {
+        scrollRect = scrollRect.GetComponent<ScrollRect>();
+        CalculateRectPos(currentLevel.value, totalLevel.value);
+    }
+    void CalculateRectPos(float currentLevel, float totalLevel)
+    {
+        float divide = currentLevel / totalLevel;
+        Debug.Log("Divide => " + divide);
+        if (divide <= .1f)
+        {
+            scrollRect.verticalNormalizedPosition = 0f;
+        }
+        else
+        {
+        scrollRect.verticalNormalizedPosition = divide;
+        }
+    }
+}
