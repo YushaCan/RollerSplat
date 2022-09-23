f6c42d74a9e9941d83efcc5123a6880d8828a025
YusaCan <ahmedyusacan@gmail.com>
2022-09-23 17:34:16 +0300

    LevelCounter Script Created
    It returns the value of the total levels. (For Level Map)

---
+++ RollerSplatPPS/Assets/Scripts/LevelCounter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCounter : MonoBehaviour
+{
+    public ScriptableInt totalLevel;
+    int otherScenes = 2;
+    void Awake()
+    {
+        totalLevel.value = SceneManager.sceneCountInBuildSettings - otherScenes;
+    }
+}
--- RollerSplatPPS/Assets/Scripts/LevelMapManager.cs
+++ RollerSplatPPS/Assets/Scripts/LevelMapManager.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class LevelMapManager : MonoBehaviour
 {
     public GameObject levelCircle;
-    public ScriptableInt curLevel;
+    public ScriptableInt curLevel, totalLevel;
     public GameObject parentGO;
     private void Awake()
     {
-        for (int i = 1; i <= curLevel.value; i++)
+        for (int i = 1; i <= totalLevel.value; i++)
         {
             GameObject go = Instantiate(levelCircle);
             go.transform.SetParent(parentGO.transform);
             go.name = "Level" + i;
             go.GetComponentInChildren<TextMeshProUGUI>().text = i.ToString();
+            if (i == curLevel.value)
+            {
+                go.GetComponent<Image>().color = Color.green;
+            }
         }
     }
 }
