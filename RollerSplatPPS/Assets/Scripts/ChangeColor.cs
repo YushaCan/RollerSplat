bc2304d95d2ac36d227b64a2633ba6d037b4e45d
YusaCan <ahmedyusacan@gmail.com>
2022-09-26 17:58:48 +0300

    Grid Color Changes With Respect To Ball Color

--- RollerSplatPPS/Assets/Scripts/ChangeColor.cs
+++ RollerSplatPPS/Assets/Scripts/ChangeColor.cs
@@ -8,17 +8,46 @@ public class ChangeColor : MonoBehaviour
     GridCell gridCell;
     MeshRenderer mesh;
     public static Action ForOccupiedCount;
+    Color gridColor;
+    public ScriptableBool red, green, blue, purple;
+    private void OnEnable()
+    {
+        RandomBallColor.changeGameGridColor += ChangeGridColor;
+    }
+    private void OnDisable()
+    {
+        RandomBallColor.changeGameGridColor -= ChangeGridColor;
+    }
     private void Awake()
     {
         gridCell = gameObject.GetComponent<GridCell>();
         mesh = gameObject.GetComponent<MeshRenderer>();
+    }
+    void ChangeGridColor()
+    {
+        if (red.isTrue)
+        {
+            gridColor = Color.red;
+        }
+        else if (blue.isTrue)
+        {
+            gridColor = Color.blue;
+        }
+        else if (green.isTrue)
+        {
+            gridColor = Color.green;
+        }
+        else if (purple.isTrue)
+        {
+            gridColor = Color.magenta;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player") && !gridCell.isOccupied)
         {
             gridCell.isOccupied = true;
-            mesh.material.color = Color.blue;
+            mesh.material.color = gridColor;
             ForOccupiedCount?.Invoke();
         }
     }
--- RollerSplatPPS/Assets/Scripts/RandomBallColor.cs
+++ RollerSplatPPS/Assets/Scripts/RandomBallColor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,36 @@ public class RandomBallColor : MonoBehaviour
     public List<Material> materials;
     int random;
     MeshRenderer materialMesh;
+    public static Action changeGameGridColor;
+    public ScriptableBool red, green, blue, purple;
+    private void Awake()
+    {
+        blue.isTrue = false;
+        green.isTrue = false;
+        purple.isTrue = false;
+        red.isTrue = false;
+    }
     void Start()
     {
-        random = Random.Range(0, materials.Count); 
+        random = UnityEngine.Random.Range(0, materials.Count); 
         materialMesh = gameObject.GetComponent<MeshRenderer>();
         materialMesh.material = materials[random];
+        if (random == 0)
+        {
+            blue.isTrue = true;
+        }
+        else if (random == 1)
+        {
+            green.isTrue = true;
+        }
+        else if (random == 2)
+        {
+            purple.isTrue = true;
+        }
+        else if (random == 3)
+        {
+            red.isTrue = true;
+        }
+        changeGameGridColor?.Invoke();
     }
 }
