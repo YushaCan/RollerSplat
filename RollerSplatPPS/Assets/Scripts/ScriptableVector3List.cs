364abfa0c92aba8f953828de377d426cca029311
YusaCan <ahmedyusacan@gmail.com>
2022-09-28 18:26:52 +0300

    Scriptable Vector3List Created
    For store the values of the path grid's pos.

---
+++ RollerSplatPPS/Assets/Scripts/ScriptableVector3List.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableVector3List", menuName = "ScriptableVector3List")]
+public class ScriptableVector3List : ScriptableObject
+{
+    public List<Vector3> vector3;
+}
